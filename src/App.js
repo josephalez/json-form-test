891f7ae12b0e158da983cbfe21bdc6ac8c9d4867
Jose Zurita <alejandrozurita999@gmail.com>
2022-11-09 15:54:55 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import JSONSchema from './schema.json';
+
+import * as toJsonSchema from '@openapi-contrib/openapi-schema-to-json-schema';
+
+
+// import { RJSFSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+import Form from "@rjsf/core";
 
 function App() {
+
+  const [schema, setSchema] = useState(null)
+
+  useEffect(() => {
+    const convertedSchema = toJsonSchema.fromSchema(JSONSchema);
+
+    console.log('pre schema', JSONSchema);
+    console.log('voncerted schema', convertedSchema);
+
+    setSchema(convertedSchema);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {schema && (
+        <Form
+          schema={{
+            ...schema.components.schemas.Adress,
+            "components": schema.components
+          }}
+          validator={validator}
+          onChange={() => console.log("changed")}
+          onSubmit={() => console.log("submitted")}
+          onError={() => console.log("errors")}
+        />
+      )}
     </div>
   );
 }
